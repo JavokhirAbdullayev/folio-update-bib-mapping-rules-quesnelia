b219456b2287d9c69fab924345602a2e42c269a2
JavokhirAbdullayev <Javokhir_Abdullaev@epam.com>
2023-12-22 01:35:07 +0500

    update tests

--- src/test/java/org/folio/util/MappingRulesUtilTest.java
+++ src/test/java/org/folio/util/MappingRulesUtilTest.java
@@ -1,6 +1,7 @@
 package org.folio.util;
 
 import com.fasterxml.jackson.databind.node.ObjectNode;
+import java.util.List;
 import org.junit.Assert;
 import org.junit.Test;
 
@@ -8,7 +9,7 @@ public class MappingRulesUtilTest {
 
     @Test
     public void shouldReplaceSpecifiedMappingRules() {
-        ObjectNode rulesReplacement = FileWorker.getJsonObject("rules/quesneliaUpdateFieldMappingRules.json");
+        List<String> rulesReplacement = List.of("100", "110", "111");
         ObjectNode mappingRulesWithCustomizations = FileWorker.getJsonObject(
           "rules/rules_with_differences_in_100_110_111.json");
         ObjectNode expectedUpdatedRules = FileWorker.getJsonObject("rules/expectedUpdatedMappingRules.json");
